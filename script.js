f24abceb581b812afeed97506ff4ada3681169c8
mohammed kaif <mkaif555123@gmail.com>
2024-05-29 12:16:24 +0500

    json add and async await performed

--- script.js
+++ script.js
@@ -7,9 +7,9 @@
 // };
 
 async function getData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+    let x =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+   let data = await x.json();
+   console.log(data);
 };
 
 
