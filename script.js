f9585a20d8428c0122572e1689e85cb9304ee2ae
mohammed kaif <mkaif555123@gmail.com>
2024-05-29 12:09:38 +0500

    .then and apis

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function getData() {
-    return new Promise((resolve , reject) =>{
-        setTimeout(() =>{
-            resolve(455)
-        }, 3000)
-    })
+// function getData() {
+//     return new Promise((resolve , reject) =>{
+//         setTimeout(() =>{
+//             resolve(455)
+//         }, 3000)
+//     })
+// };
+
+async function getData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
 };
 
 
-async function main(){
-console.log("load data");
+async function main() {
+    console.log("load data");
 
-let data =  await getData();
-console.log(data);
-console.log("process data");
-console.log("task 2");
+    let data = await getData();
+    console.log(data);
+    console.log("process data");
+    console.log("task 2");
 }
 
 main()
