79f503a5b4072c03c9d029cc937727ddaa4f5352
mohammed kaif <mkaif555123@gmail.com>
2024-05-29 14:31:32 +0500

    using try catch

--- script.js
+++ script.js
@@ -6,20 +6,44 @@
 //     })
 // };
 
-async function getData() {
-    let x =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
-   let data = await x.json();
-   console.log(data);
-};
+
+//ASYNC AWAIT 
+// async function getData() {
+//     let x =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//    let data = await x.json();
+//    console.log(data);
+// };
 
 
-async function main() {
-    console.log("load data");
+// async function main() {
+//     console.log("load data");
 
-    let data = await getData();
-    console.log(data);
-    console.log("process data");
-    console.log("task 2");
+//     let data = await getData();
+//     console.log(data);
+//     console.log("process data");
+//     console.log("task 2");
+// }
+
+// main()
+
+//TRY CATCH
+
+setTimeout(() =>{
+    console.log("loading your wifi .. please wait ...!");
+} , 1000)
+
+
+try{
+
+    console.log(kaif);
+}catch(error){
+    console.log(error);
 }
 
-main()
+setTimeout(() =>{
+    console.log("detect your virus in wifi .. please wait ...!");
+} , 2000)
+
+setTimeout(() =>{
+    console.log("confirmed connect in  your wifi .. you are login");
+} , 5000)
